fa222f10e69bb6abea9c66ca213cf4923f7b1e27
sri0711 <m.sathish0711@gmail.com>
2025-02-14 00:55:03 +0530

    another api fix

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import config from "./Helpers/config";
 function App() {
   const [message, setMessage] = useState("");
   const UpdateStateCheck = () => {
+    console.log("config.SERVER_URL");
     fetch(config.SERVER_URL).then((data) => {
       data.json().then((json) => {
         setMessage(json.data);
