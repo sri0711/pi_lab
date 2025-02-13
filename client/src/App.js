424d9e49b8586a1200364ab4a2fa7a2cfa21b5a4
sri0711 <m.sathish0711@gmail.com>
2025-02-13 23:20:55 +0530

    network Fix

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState("");
   const UpdateStateCheck = () => {
-    fetch("http://localhost:3001").then((data) => {
+    fetch("http://server:3001").then((data) => {
       data.json().then((json) => {
         setMessage(json.data);
       });
