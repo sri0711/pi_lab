04604b0d147d36dae57f3c2026cd644c9e6632bb
sri0711 <m.sathish0711@gmail.com>
2025-02-14 02:39:30 +0530

    ipUpdated

--- client/src/Helpers/config.js
+++ client/src/Helpers/config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  SERVER_URL: process.env.NODE_SERVER + "/" || "http://localhost:3001/",
+  SERVER_URL: process.env.NODE_SERVER || "http://server:3001/",
 };
